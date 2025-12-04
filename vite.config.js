52eadb6e4a1417470bcb37d82a2db0aefcd2e76e
samu777684 <varonmateo042@gmail.com>
2025-12-03 22:19:29 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto-frontend/',  // nombre exacto de tu repo
-})
+  base: './'
+});
