e4dbde26dc9b4e76b41565154d918f44ea97362c
samu777684 <varonmateo042@gmail.com>
2025-12-04 16:26:05 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // ¡Importantísimo! No './'
-})
+  server: {
+    proxy: {
+      '/api': 'https://proyecto-2-yy3f.onrender.com'
+    }
+  }
+});
